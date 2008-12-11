7eacb101649f108a16a45fa1ee0de3db212d610a
KennethLavrsen <KennethLavrsen@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2008-12-11 23:59:58 +0000

    Item433: Rebrand ChecklistPlugin
    git-svn-id: http://svn.foswiki.org/trunk/ChecklistPlugin@1270 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Plugins/ChecklistPlugin/build.pl
+++ lib/Foswiki/Plugins/ChecklistPlugin/build.pl
@@ -8,10 +8,10 @@ BEGIN {
   }
 }
 
-use TWiki::Contrib::Build;
+use Foswiki::Contrib::Build;
 
 # Create the build object
-$build = new TWiki::Contrib::Build( 'ChecklistPlugin' );
+$build = new Foswiki::Contrib::Build( 'ChecklistPlugin' );
 
 # Build the target on the command line, or the default target
 $build->build($build->{target});
